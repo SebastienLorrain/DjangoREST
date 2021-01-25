dd51c665dc5c9ce1ee38ff11cb449a82815267a1
Sebastien Lorrain <sebastien.lorrain@gmail.com>
2021-01-25 17:15:00 +0100

    Removed test code for TravisCI

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
-from os import path as ospath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
